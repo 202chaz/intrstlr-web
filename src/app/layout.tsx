9e9b5bb0fb58b072553d76722f6e0255d862c99f
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2023-12-22 19:20:45 -0500

    Update homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:url" content="https://intrstlr.com/" />
+        <meta property="og:site_name" content="Intrstlr" />
+        <meta property="og:title" content="Intrstlr | Engineers of Tomorrow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Intrstlr presents the Candle Collection." />
+        <meta property="og:image" content="https://intrstlr.nyc3.cdn.digitaloceanspaces.com/three_candles.png" />
+        <title>Intrstlr</title>
+        <meta name="description" content="Intrstlr presents the Candle Collection." />
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="https://intrstlr.com" />
+        <meta property="twitter:url" content="https://intrstlr.com" />
+        <meta name="twitter:title" content="Intrstlr | Engineers of Tomorrow" />
+        <meta name="twitter:description" content="Intrstlr presents the Candle Collection." />
+        <meta name="twitter:image" content="https://intrstlr.nyc3.cdn.digitaloceanspaces.com/three_candles.png" />
+      </head>
       <body className={roboto.className}>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,60 +32,112 @@ export default function Home() {
   let cursorSpan: any;
 
   const erase = () => {
-	if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if(textArrayIndex>=textArray.length) textArrayIndex=0;
-    setTimeout(type, typingDelay + 1100);
+    if (charIndex > 0) {
+      if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+      typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+      charIndex--;
+      setTimeout(erase, erasingDelay);
+    }
+    else {
+      cursorSpan.classList.remove("typing");
+      textArrayIndex++;
+      if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+      setTimeout(type, typingDelay + 1100);
+    }
   }
-}
 
   const type = () => {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-  	setTimeout(erase, newTextDelay);
+    if (charIndex < textArray[textArrayIndex].length) {
+      if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+      typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingDelay);
+    }
+    else {
+      cursorSpan.classList.remove("typing");
+      setTimeout(erase, newTextDelay);
+    }
   }
-}
+
+  const startCountdown = () => {
+    const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+    //I'm adding this section so I don't have to keep updating this pen every year :-)
+    //remove this if you don't need it
+    let today = new Date() as any,
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      nextYear = yyyy + 1,
+      dayMonth = "02/01/",
+      birthday = dayMonth + yyyy;
+
+    today = mm + "/" + dd + "/" + yyyy;
+    if (today > birthday) {
+      birthday = dayMonth + nextYear;
+    }
+    //end
+
+    const countDown = new Date(birthday).getTime(),
+      x = setInterval(function () {
+
+        const now = new Date().getTime(),
+          distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          document.getElementById("headline").innerText = "It's my birthday!";
+          document.getElementById("countdown").style.display = "none";
+          document.getElementById("content").style.display = "block";
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }
 
   useEffect(() => {
+    startCountdown()
     typedTextSpan = document.querySelector(".typed-text");
     cursorSpan = document.querySelector(".cursor");
-    if(textArray.length) setTimeout(type, newTextDelay + 250)
+    if (textArray.length) setTimeout(type, newTextDelay + 250)
   }, [])
 
   return (
     <>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Handjet:wght@700;900&display=swap" rel="stylesheet" />
-      <audio src="https://intrstlr.nyc3.cdn.digitaloceanspaces.com/crackling.mp3" loop autoPlay controlsList="nodownload" />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h2 className="main-heading">First Encounter</h2>
-        <div className="typewriter">
-          <p>I Can <span className="typed-text"></span><span className="cursor">&nbsp;</span></p>
+      <div className="video-container">
+        <video id="background-video" className='background-video' autoPlay loop muted poster="https://assets.codepen.io/6093409/river.jpg">
+          <source src="https://intrstlr.nyc3.cdn.digitaloceanspaces.com/intrstlr_add.mp4" type="video/mp4" />
+        </video>
+        <div className="container">
+          <h1 id="headline">LAUNCHING</h1>
+          <div id="countdown">
+            <ul>
+              <li><span id="days"></span>days</li>
+              <li><span id="hours"></span>Hours</li>
+              <li><span id="minutes"></span>Minutes</li>
+              <li><span id="seconds"></span>Seconds</li>
+            </ul>
+          </div>
+          <div id="content" className="emoji">
+            <span>🥳</span>
+            <span>🎉</span>
+            <span>🎂</span>
+          </div>
         </div>
-
-        <div className="main-container">
-          <Image 
-            alt="Picture of candle"
-            src="https://intrstlr.nyc3.cdn.digitaloceanspaces.com/cndl.png" 
-            width={500}
-            height={500}
-          />
+        <div>
+          <div className="typewriter">
+            <p>I Can <span className="typed-text"></span><span className="cursor">&nbsp;</span></p>
+          </div>
         </div>
-      </main>
+      </div>
     </>
   )
 }
